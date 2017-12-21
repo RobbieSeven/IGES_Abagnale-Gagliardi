88e4671e34a38fb0ae7e5bdf6e4539d5311390fa
broke31 <giammagiorda@gmail.com>
2017-12-21 16:47:15 +0100

    cambiata la versione di base sdk, siamo passati dalla 21 alla 19 per maggiore compatibilita'

--- app/src/main/java/com/giordanogiammaria/microapp30/SplashScreen.java
+++ app/src/main/java/com/giordanogiammaria/microapp30/SplashScreen.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 
 public class SplashScreen extends AppCompatActivity {
     /*splashscreen */
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
