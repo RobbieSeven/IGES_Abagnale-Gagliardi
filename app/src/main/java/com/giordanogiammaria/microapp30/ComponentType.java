7c7b3199788d42e58ddcf8fe9a698801c002090c
Abagnale92 <abagnale92@gmail.com>
2018-01-22 10:21:50 +0100

    ComponentType DataType

---
+++ app/src/main/java/com/giordanogiammaria/microapp30/ComponentType.java
@@ -0,0 +1,20 @@
+package com.giordanogiammaria.microapp30;
+
+/**
+ * Created by Giuseppe Abagnale on 15/01/2018.
+ */
+
+public enum ComponentType {
+
+    TAKEPHOTO,
+    PREVIEWIMAGE,
+    CALLCONTACT,
+    CALLINTERCEPTOR,
+    SELECTCONTACT,
+    SENDMESSAGE,
+    LOCATION,
+    MAP,
+    SAVE,
+    SENDMAIL
+
+}
---
+++ app/src/main/java/com/giordanogiammaria/microapp30/DataType.java
@@ -0,0 +1,15 @@
+package com.giordanogiammaria.microapp30;
+
+/**
+ * Created by Giuseppe Abagnale on 15/01/2018.
+ */
+
+public enum DataType {
+
+    CONTACT,
+    STRING,
+    LOCATION,
+    IMAGE,
+    URI,
+    OBJECT
+}
