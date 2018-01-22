081c69483db0e60631c6cc1979363a1834d2249f
broke31 <giammagiorda@gmail.com>
2018-01-22 09:46:50 +0100

    aggiunto il logo e l'alert dialog alla pressione del tasto back

--- app/src/main/java/com/giordanogiammaria/microapp30/MainActivity.java
+++ app/src/main/java/com/giordanogiammaria/microapp30/MainActivity.java
@@ -1,10 +1,12 @@
 package com.giordanogiammaria.microapp30;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -47,5 +49,22 @@ public class MainActivity extends AppCompatActivity {
                 return;
             }
         }
+    }
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("Closing Activity")
+                .setMessage("Are you sure you want to close this activity?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+
+                })
+                .setNegativeButton("No", null)
+                .show();
     }
 }
--- app/src/main/java/com/giordanogiammaria/microapp30/MicroAppActivity.java
+++ app/src/main/java/com/giordanogiammaria/microapp30/MicroAppActivity.java
@@ -3,11 +3,11 @@ package com.giordanogiammaria.microapp30;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MAActivity extends AppCompatActivity {
+public class MicroAppActivity extends AppCompatActivity {
     // modifica di Rob
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ma);
+        setContentView(R.layout.activity_maicro_app);
     }
 }
